27c726a711ff27f4b9c2e55309d5d40a2c0ee26b
DESKTOP-7C0GO30\lisac <lkctec8@gmail.com>
2025-11-02 20:39:20 +0000

    Initial commit

---
+++ src/GildedRoseTests/ApprovalTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class ApprovalTest
+{
+    [Fact]
+    public Task ThirtyDays()
+    {
+        var fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader("a\n"));
+
+        Program.Main(["30"]);
+        var output = fakeOutput.ToString();
+
+        return Verifier.Verify(output);
+    }
+}
---
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,15 @@
+﻿using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void Foo()
+    {
+        List<Item> items = [ new Item { Name = "foo", SellIn = 0, Quality = 0 } ];
+        GildedRose app = new(items);
+        app.UpdateQuality();
+        Assert.Equal("fixme", items[0].Name);
+    }
+}
